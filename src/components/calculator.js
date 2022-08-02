a677a28d5ed5af56a0b07423c8a7969cb310b937
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-02 19:38:03 +0200

    Trying to make work the calculator.js

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,41 +1,33 @@
 import React from 'react';
+import calculate from './logic/calculate';
 
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      first: true,
+        total: '',
+        next: '',
+        operation: '',
     };
+    his.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
+    const buttonName = e.target.name;
+    this.setState((prevState) => ({
+      ...calculate(prevState, buttonName),
+    }));
   }
 
   render() {
-    const { first } = this.state;
+    const { total, next, operation } = this.state;
     return (
       <div className="calculator-container">
         <div className="cal-screen">
-          <span>0</span>
+        <span>{ next || operation || total || ''}</span>
         </div>
         <form className="calculator-digits" name="calculator">
-          {first}
-          <input type="button" value="AC" />
-          <input type="button" value="+/-" />
-          <input type="button" value="%" />
-          <input type="button" value={'\u00f7'} className="peach-color divide" />
-          <input type="button" value="7" />
-          <input type="button" value="8" />
-          <input type="button" value="9" />
-          <input type="button" value={'\u00D7'} className="peach-color" />
-          <input type="button" value="4" />
-          <input type="button" value="5" />
-          <input type="button" value="6" />
-          <input type="button" value={'\u2212'} className="peach-color" />
-          <input type="button" value="3" />
-          <input type="button" value="2" />
-          <input type="button" value="1" />
-          <input type="button" value={'\u1429'} className="peach-color" />
-          <input type="button" value="0" className="zero" />
-          <input type="button" value="." />
-          <input type="button" value={'\u003D'} className="peach-color" />
+          
         </form>
       </div>
     );
