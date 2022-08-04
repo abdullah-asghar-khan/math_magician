0f7b52af95442224e80ff0e85df48a8c6a9d2097
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-04 09:09:51 +0200

    Addidng refractor elements

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import React from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      first: '',
-    };
-  }
-
-  render() {
-    const { first } = this.state;
-    return (
-      <div className="App">
-        <Calculator name={first} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
 }
 
 export default App;
