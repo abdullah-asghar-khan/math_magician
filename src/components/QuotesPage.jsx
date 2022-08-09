09c6d4592e805fbefb7cdb7864f791798a017cc2
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-09 10:43:55 +0200

    Quotes page added, styling left

---
+++ src/components/QuotesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const QuotesPage = () => (
+  <div className="quotes-page">
+    <h3>
+    Without mathematics, there’s nothing you can do.
+      <br />
+      Everything around you is mathematics. Albert Einstein
+    </h3>
+  </div>
+);
+
+export default QuotesPage;
