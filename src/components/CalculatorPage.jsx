e2c1aa2b09a5c4603e24f89c5ea968f7284026dd
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-09 10:34:37 +0200

    Adding new components

---
+++ src/components/CalculatorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calculator from './calculator';
+
+const CalculatorPage = () => (
+  <div className="calc-page">
+    <h3>Lets do some math!</h3>
+    <Calculator />
+  </div>
+);
+
+export default CalculatorPage;
