7428cb2a9ad575e207b6d722185637d1fd2d1ba0
Abdullah2213565 <dulakhan024@gmail.com>
2022-08-10 10:21:44 +0200

    Adding testing for components

---
+++ src/components/test/CalculatorPage.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import CalculatorPage from '../CalculatorPage';
+
+test('test id Calculator page is rendered', () => {
+  const CalcPage = renderer.create(<CalculatorPage />).toJSON();
+  expect(CalcPage).toMatchSnapshot();
+})
